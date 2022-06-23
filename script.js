36069143539621cd6c42997041af6a6258ee9fb2
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-23 10:37:52 -0300

    Done

---
+++ script.js
@@ -0,0 +1,26 @@
+// ### EXERCÍCIO 1 ###
+
+let series = ["Peaky Blinders", "Stranger Things", "The Boys", "House M.D", "Friends"];
+console.log(series);
+
+// ### EXERCÍCIO 2 ###
+
+let friends = ["Ross", "Rachel", "Chandler", "Mônica", "Joey"];
+friends.unshift("Phoebe");
+console.log(friends);
+
+// ### EXERCÍCIO 3 ###
+
+friends.pop();
+console.log(friends);
+
+// ### EXERCÍCIO 4 ###
+
+friends.shift();
+console.log(friends);
+
+// ### EXERCÍCIO 5 ###
+
+let numbers = [7,5,6,3,8,9,2,1,4];
+console.log(numbers);
+console.log("Número em ordem numérica:", numbers.sort());
